0e6cafac2f5ca3adf36ec18a45bdf6594d03f1b1
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-02-02 21:10:32 +0100

    Refactor lib/message module a bit

---
+++ lib/message/index.ts
@@ -0,0 +1,4 @@
+export type { ClientMessage } from "./client-message";
+export { parseClientMessage } from "./client-message";
+export type { ServerMessage } from "./server-message";
+export { parseServerMessage } from "./server-message";
---
+++ lib/message/server-message.ts
@@ -0,0 +1,43 @@
+import z from "zod";
+
+/**
+ * Message for the server.
+ * Sent from a client to the server.
+ */
+export type ServerMessage =
+    | {
+          /**
+           * A message sent from a client.
+           * Server should distribute it among all connected clients.
+           */
+          Message: {
+              userId: number;
+              content: string;
+          };
+      }
+    // TODO
+    | {};
+
+const ServerMessageSchema: z.ZodSchema<ServerMessage> = z.union([
+    z.object({
+        Message: z.object({
+            userId: z.number(),
+            content: z.string(),
+        }),
+    }),
+    // TODO
+    z.object({}),
+]);
+
+/**
+ * Attempts to parse the given message string as a server message.
+ */
+export function parseServerMessage(raw: string): ServerMessage | null {
+    const result = ServerMessageSchema.safeParse(raw);
+    if (result.success) {
+        return result.data;
+    } else {
+        console.error(result.error);
+        return null;
+    }
+}
