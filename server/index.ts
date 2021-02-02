d3c8a645356289c0b2e88381abe5d962324bf783
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-02-02 19:35:29 +0100

    Initial commit
    Setup NextJS app and TS express server

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen(8090, () => console.log("server running"));
+
+console.log("server?");
+
+// export default function () {
+//     console.log("hello server!");
+// }
