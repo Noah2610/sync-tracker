f2489f66a08be1b8c54da2a6139878ddca8a7a9e
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-02-03 03:43:43 +0100

    Quick and dirty chat app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { parseClientMessage } from "../../lib/message";
 import sendMessage from "../send-message";
 
-// const WS_PORT = 8090;
-
 export default function Home() {
     const [ws, setWs] = useState<WebSocket | null>(null);
+    const [clientId, setClientId] = useState<number | null>(null);
+    const [messages, setMessages] = useState<
+        {
+            clientId: number;
+            content: string;
+        }[]
+    >([]);
 
-    useEffect(() => {
-        // const ws = new WebSocket(`ws://${location.hostname}:${WS_PORT}`);
-        const ws = new WebSocket("wss://sync-tracker-ws.herokuapp.com");
+    const messageContentRef = useRef("");
+
+    const addMessage = (clientId: number, content: string) =>
+        setMessages((prev) => [...prev, { clientId, content }]);
+
+    const connectWs = () => {
+        let ws: WebSocket | null = null;
+
+        try {
+            ws = new WebSocket("wss://sync-tracker-ws.herokuapp.com");
+        } catch (e) {
+            console.error(e);
+        }
+
+        if (!ws) {
+            console.warn("Retrying WebSocket connection in 2 seconds...");
+            setTimeout(connectWs, 2000);
+            return;
+        }
 
         ws.addEventListener("message", (event) => {
             if (event.data) {
@@ -17,13 +38,11 @@ export default function Home() {
                 if (message) {
                     switch (message.kind) {
                         case "Connected": {
-                            console.log(`Connected as user ${message.id}`);
+                            setClientId(message.id);
                             break;
                         }
                         case "Message": {
-                            console.log(
-                                `<${message.userId}> ${message.content}`,
-                            );
+                            addMessage(message.userId, message.content);
                             break;
                         }
                         default: {
@@ -35,24 +54,63 @@ export default function Home() {
         });
 
         setWs(ws);
+    };
 
-        return () => ws.close();
+    useEffect(() => {
+        connectWs();
+        return () => {
+            if (ws) {
+                ws.close();
+            }
+        };
     }, []);
+
+    if (!ws) {
+        return <>Attempting to connect to WebSocket...</>;
+    }
 
     return (
         <>
-            {ws && (
-                <button
-                    onClick={() =>
+            {clientId && (
+                <div>
+                    Your client ID: <strong>{clientId}</strong>
+                </div>
+            )}
+
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    if (messageContentRef.current) {
                         sendMessage(ws, {
                             kind: "Message",
-                            content: "hello",
-                        })
+                            content: messageContentRef.current,
+                        });
                     }
-                >
-                    Click me!
-                </button>
-            )}
+                }}
+            >
+                <div>
+                    <label htmlFor="messageContent">Message: </label>
+                    <input
+                        type="text"
+                        name="messageContent"
+                        onKeyUp={(e) =>
+                            (messageContentRef.current = e.currentTarget.value)
+                        }
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Send Message" />
+                </div>
+            </form>
+
+            <div>
+                {messages.map((message, i) => (
+                    <div key={i}>
+                        <strong>&lt;{message.clientId}&gt;</strong>{" "}
+                        <span>{message.content}</span>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
