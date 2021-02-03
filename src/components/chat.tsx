8befa9b9df2198bb99ab427bac38ee8af7eb2c31
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-02-03 21:52:15 +0100

    Add Chat component, use new WsState WS abstraction

---
+++ src/components/chat.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState, useRef } from "react";
+import { useWs } from "../ws/ws-context";
+
+export default function Chat() {
+    const ws = useWs();
+    const [clientId, setClientId] = useState<number | null>(null);
+    const [messages, setMessages] = useState<
+        {
+            clientId: number;
+            content: string;
+        }[]
+    >([]);
+
+    const messageContentRef = useRef("");
+
+    const addMessage = (clientId: number, content: string) =>
+        setMessages((prev) => [...prev, { clientId, content }]);
+
+    useEffect(() => {
+        if (ws) {
+            ws.messages.on("Connected", (message) => setClientId(message.id));
+            ws.messages.on("Message", (message) =>
+                addMessage(message.clientId, message.content),
+            );
+        }
+    }, [ws]);
+
+    if (!ws) {
+        return <>No WsSocket connection.</>;
+    }
+
+    return (
+        <>
+            {clientId && (
+                <div>
+                    Your client ID: <strong>{clientId}</strong>
+                </div>
+            )}
+
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    if (messageContentRef.current) {
+                        ws.sendMessage({
+                            kind: "Message",
+                            content: messageContentRef.current,
+                        });
+                    }
+                }}
+            >
+                <div>
+                    <label htmlFor="messageContent">Message: </label>
+                    <input
+                        type="text"
+                        name="messageContent"
+                        onKeyUp={(e) =>
+                            (messageContentRef.current = e.currentTarget.value)
+                        }
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Send Message" />
+                </div>
+            </form>
+
+            <div>
+                {messages.map((message, i) => (
+                    <div key={i}>
+                        <strong>&lt;{message.clientId}&gt;</strong>{" "}
+                        <span>{message.content}</span>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
--- src/send-message.ts
+++ src/send-message.ts
@@ -4,6 +4,5 @@ import { ServerMessage } from "../lib/message";
  * Send the given message to the WS server.
  */
 export default function sendMessage(ws: WebSocket, message: ServerMessage) {
-    console.log(message);
     ws.send(JSON.stringify(message));
 }
