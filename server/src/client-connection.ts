3df1ee87da39e2cb8e83812c204ceb8a32d62838
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-02-04 18:01:05 +0100

    Add newClientConnection function to client-connection module
    Also generates a random default name.

--- server/src/client-connection.ts
+++ server/src/client-connection.ts
@@ -1,7 +1,81 @@
-import Client from "../../lib/client";
 import WebSocket from "ws";
+import Client from "../../lib/client";
+import State from "./state";
 
 export default interface ClientConnection {
     client: Client;
     ws: WebSocket;
+}
+
+/**
+ * Create a new `ClientConnection` with the given WebSocket.
+ */
+export function newClientConnection(
+    state: State,
+    ws: WebSocket,
+): ClientConnection {
+    return {
+        client: newClient(state),
+        ws,
+    };
+}
+
+function newClient(state: State): Client {
+    return {
+        id: Math.max(...state.connections.map((conn) => conn.client.id), 0) + 1,
+        name: newClientName(state),
+    };
+}
+
+function newClientName(state: State): string {
+    const NAMES = [
+        "Composer",
+        "Writer",
+        "Tracker",
+        "Creator",
+        "Dude",
+        "Worker",
+        "Genius",
+        "Master",
+        "Addict",
+        "Player",
+        "Pianist",
+        "Guitarist",
+        "Artist",
+    ];
+    const ADJECTIVES = [
+        "Music",
+        "Passionate",
+        "Lovable",
+        "Likable",
+        "Amazing",
+        "Cool",
+        "Unstoppable",
+        "Unbeatable",
+        "Perfect",
+        "Go-getter",
+        "Junior",
+        "Senior",
+        "Hardcore",
+        "Genius",
+        "Strong",
+        "Grand",
+    ];
+    const MAX_LOOP = 100;
+
+    const pick = (list: string[]): string =>
+        list[Math.floor(Math.random() * list.length)];
+
+    const genName = (): string => `${pick(ADJECTIVES)} ${pick(NAMES)}`;
+
+    const isNameTaken = (name: string): boolean =>
+        !!state.connections.find((conn) => conn.client.name === name);
+
+    let name = genName();
+    let i = 0;
+    while (isNameTaken(name) && i < MAX_LOOP) {
+        name = genName();
+        i++;
+    }
+    return name;
 }
--- server/src/state.ts
+++ server/src/state.ts
@@ -1,11 +1,16 @@
-import ClientConnection from "./client-connection";
+import WebSocket from "ws";
+import ClientConnection, { newClientConnection } from "./client-connection";
 
 export default interface State {
     connections: ClientConnection[];
+    newClientConnection(ws: WebSocket): void;
 }
 
 export function createState(): State {
     return {
         connections: [],
+        newClientConnection(ws) {
+            newClientConnection(this, ws);
+        },
     };
 }
