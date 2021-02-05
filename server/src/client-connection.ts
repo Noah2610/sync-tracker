25eb5c13d50064b29c5cfb06ce5d1290fe4d9f04
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-02-05 04:19:22 +0100

    Move generated client name's names and adjectives to JSON file
    Also add some more available names and adjectives.

--- server/src/client-connection.ts
+++ server/src/client-connection.ts
@@ -2,6 +2,8 @@ import WebSocket from "ws";
 import Client from "../../lib/client";
 import State from "./state";
 
+import clientNames from "./client-names.json";
+
 export default interface ClientConnection {
     client: Client;
     ws: WebSocket;
@@ -42,50 +44,18 @@ export function removeClientConnection(state: State, clientId: number) {
 function newClient(state: State): Client {
     return {
         id: Math.max(...state.connections.map((conn) => conn.client.id), 0) + 1,
-        name: newClientName(state),
+        name: generateClientName(state),
     };
 }
 
-function newClientName(state: State): string {
-    const NAMES = [
-        "Composer",
-        "Writer",
-        "Tracker",
-        "Creator",
-        "Dude",
-        "Worker",
-        "Genius",
-        "Master",
-        "Addict",
-        "Player",
-        "Pianist",
-        "Guitarist",
-        "Artist",
-    ];
-    const ADJECTIVES = [
-        "Music",
-        "Passionate",
-        "Lovable",
-        "Likable",
-        "Amazing",
-        "Cool",
-        "Unstoppable",
-        "Unbeatable",
-        "Perfect",
-        "Go-getter",
-        "Junior",
-        "Senior",
-        "Hardcore",
-        "Genius",
-        "Strong",
-        "Grand",
-    ];
+function generateClientName(state: State): string {
     const MAX_LOOP = 100;
 
     const pick = (list: string[]): string =>
         list[Math.floor(Math.random() * list.length)];
 
-    const genName = (): string => `${pick(ADJECTIVES)} ${pick(NAMES)}`;
+    const genName = (): string =>
+        `${pick(clientNames.adjectives)} ${pick(clientNames.names)}`;
 
     const isNameTaken = (name: string): boolean =>
         !!state.connections.find((conn) => conn.client.name === name);
